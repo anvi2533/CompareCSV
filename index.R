0ac6379e054ddf78f084a7f7571b6bc8e5799aa6
Anders Viklund <anders.viklund@gmail.com>
2021-12-20 15:36:39 +0100

    2112201536

--- index.R
+++ index.R
@@ -1,44 +1,77 @@
 #install.packages("dplyr")
 #install.packages("generics")
 
-file_1 <- data.frame(read.csv("CSV_files/DevicesWithInventory_4cec84c7-05ef-4a99-94c3-5a7313aee86a.csv"))
-file_2 <- data.frame(read.csv("CSV_files/DevicesWithInventory_ea5cdce2-43db-4f36-8a32-1e0c0720b08a.csv"))
+file_1 <- data.frame(read.csv("CSV_files/DevicesWithInventory_ea5cdce2-43db-4f36-8a32-1e0c0720b08a.csv"))
+file_2 <- data.frame(read.csv("CSV_files/DevicesWithInventory_202c13df-3905-483e-b537-16f52eb1ca66.csv"))
 
 nrow(file_1)
 nrow(file_2)
 
-id <- file_1$ï..Device.ID
 name <- file_1$Primary.user.display.name
 serial <- file_1$Serial.number
-comp_name <- file_1$Device.name
-manufact <- file_1$Manufacturer
-enrollment <- file_1$Enrollment.date
-model <- file_1$Model
+part_1 <- data.frame(name,serial)
 
-file_1 <- data.frame(id,name,serial,comp_name,manufact,enrollment,model)
-
-id <- file_2$ï..Device.ID
 name <- file_2$Primary.user.display.name
 serial <- file_2$Serial.number
-comp_name <- file_2$Device.name
-manufact <- file_2$Manufacturer
-enrollment <- file_2$Enrollment.date
-model <- file_2$Model
+part_2 <- data.frame(name,serial)
 
-file_2 <- data.frame(id,name,serial,comp_name,manufact,enrollment,model)
+for (col in colnames(part_1)) {
+  Encoding(part_1[[col]]) <- "UTF-8"
+}
+
+for (col in colnames(part_2)) {
+  Encoding(part_2[[col]]) <- "UTF-8"
+}
 
 #Compare file 1 and file 2
 
 require(dplyr)
 require(generics)
 
-a <- anti_join(file_1,file_2)
-b <- anti_join(file_2,file_1)
+a <- anti_join(part_1,part_2)
+b <- anti_join(part_2,part_1)
 c <- rbind(a,b)
 
 #matching rows
-d <- generics::intersect(file_1,file_2)
+d <- generics::intersect(part_1,part_2)
 write.csv(d,"matching_rows.csv", row.names = FALSE)
+paste(nrow(d),"identical rows")
 
 #mismatching rows
 write.csv(c,"mismatching_rows.csv", row.names = FALSE)
+paste(nrow(a), "rows in OLD file but not in NEW")
+paste(nrow(b), "rows in NEW file but not in OLD")
+
+serial_model <- file_1[,c(9,11)]
+names(serial_model)[1] <- "serial"
+names(serial_model)[2] <- "model"
+serial_model_1 <- serial_model
+
+serial_model <- file_2[,c(9,11)]
+names(serial_model)[1] <- "serial"
+names(serial_model)[2] <- "model"
+serial_model_2 <- serial_model
+
+a <- left_join(a,serial_model_1)
+a$type <- "in old not in new"
+
+b <- left_join(b,serial_model_2)
+b$type <- "in new not in old"
+
+rbind(a,b)
+
+
+
+
+
+
+
+
+i = "Johanna Hartman"
+
+part_1[part_1$name == i,]
+part_2[part_2$name == i,]
+
+
+
+
